75f795a06d33765e813edb28d1333084908292ff
gourmetseasoningsake <jerome@shtill.com>
2022-04-24 20:07:27 +0200

    files safu

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { minifyHTMLLiterals } from "minify-html-literals";
+import { createFilter } from "@rollup/pluginutils";
+
+export default (options = {}) => {
+
+    if(!options.minifyHTMLLiterals) {
+        options.minifyHTMLLiterals = minifyHTMLLiterals;
+    }
+
+    if(!options.filter) {
+        options.filter = createFilter(options.include, options.exclude);
+    }
+
+    const minifyOptions = options.options || {};
+
+    return {
+        name: "html-literals",
+
+        transform(code, id) {
+            if(!options.filter(id)) return null;
+
+            try {
+                return options.minifyHTMLLiterals(code, { ...minifyOptions, fileName: id });
+            } catch (error) {
+                if(options.failOnError) {
+                    this.error(error.message);
+                } else {
+                    this.warn(error.message);
+                }
+            }
+        }
+    };
+};
